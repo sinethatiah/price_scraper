2a5e08c16ee7f7dc1f771475cafab73cdf9d4cdd
thatiah <thatiahnia@gmail.com>
2026-06-15 09:07:53 +0300

    scrape the website to get books and prices

--- PriceScraper.py
+++ PriceScraper.py
@@ -2,4 +2,14 @@ import requests
 from bs4 import  BeautifulSoup
 import csv
 
+url="https://books.toscrape.com/"
+response= requests.get(url)
+soup= BeautifulSoup(response.text, "html.parser")
 
+Book_data=[]
+books=soup.find_all("article" , class_="product_pod")
+
+for book in books:
+    book_title=book.h3.a["title"]
+    book_price=book.find("p", class_="price_color").text
+    print(f"{book_price},{book_title}")
