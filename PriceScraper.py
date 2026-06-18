7ba83d61eb0cb4a9e30cca1e24bfe3cb82d4122b
thatiah <thatiahnia@gmail.com>
2026-06-18 19:50:44 +0300

    convert prices to kenyan shillings

--- PriceScraper.py
+++ PriceScraper.py
@@ -2,6 +2,12 @@ import requests
 from bs4 import  BeautifulSoup
 import csv
 
+API_KEY="48cb79b886cfdf2c5b39e959"
+exchange_url = f"https://v6.exchangerate-api.com/v6/{API_KEY}/pair/GBP/KES"
+exchange_response=requests.get(exchange_url)
+rate = exchange_response.json()["conversion_rate"]
+
+
 url="https://books.toscrape.com/"
 response= requests.get(url)
 soup= BeautifulSoup(response.text, "html.parser")
@@ -13,3 +19,6 @@ for book in books:
     book_title=book.h3.a["title"]
     book_price=book.find("p", class_="price_color").text
     print(f"{book_price},{book_title}")
+
+
+
