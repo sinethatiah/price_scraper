b19fa90beb00d0cc51d34e800fc956d0f9d07a3e
thatiah <thatiahnia@gmail.com>
2026-06-18 20:03:54 +0300

    change format of the price to enable conversion

--- PriceScraper.py
+++ PriceScraper.py
@@ -1,24 +1,26 @@
 import requests
-from bs4 import  BeautifulSoup
+from bs4 import BeautifulSoup
 import csv
 
-API_KEY="48cb79b886cfdf2c5b39e959"
+API_KEY = "48cb79b886cfdf2c5b39e959"
 exchange_url = f"https://v6.exchangerate-api.com/v6/{API_KEY}/pair/GBP/KES"
-exchange_response=requests.get(exchange_url)
+exchange_response = requests.get(exchange_url)
 rate = exchange_response.json()["conversion_rate"]
 
+url = "https://books.toscrape.com/"
+response = requests.get(url)
+response.encoding = "utf-8"  # fix encoding here
+soup = BeautifulSoup(response.text, "html.parser")
 
-url="https://books.toscrape.com/"
-response= requests.get(url)
-soup= BeautifulSoup(response.text, "html.parser")
-
-Book_data=[]
-books=soup.find_all("article" , class_="product_pod")
+book_data = []
+books = soup.find_all("article", class_="product_pod")
 
 for book in books:
-    book_title=book.h3.a["title"]
-    book_price=book.find("p", class_="price_color").text
-    print(f"{book_price},{book_title}")
+    book_title = book.h3.a["title"]
+    raw_price = book.find("p", class_="price_color").text
+    price_gbp = float(raw_price.replace("£", "").replace("Â£", "").strip())
+    price_kes = round(price_gbp * rate, 2)
+    print(f"{book_title} | £{price_gbp} | KES {price_kes}")
 
 
 
